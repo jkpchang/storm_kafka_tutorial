4cb9e2f42c8a53c6b9fb3b7d02cd6eade26419a8
Jorge Chang <jorge@jorgechang.com>
2015-04-03 15:02:58 -0700

    demo schema

--- src/main/java/com/hortonworks/tutorials/tutorial3/DemoScheme.java
+++ src/main/java/com/hortonworks/tutorials/tutorial3/DemoScheme.java
@@ -1,9 +1,7 @@
 package com.hortonworks.tutorials.tutorial3;
 
-import java.io.UnsupportedEncodingException;
-import java.sql.Timestamp;
 import java.util.List;
-
+import org.json.*;
 import org.apache.log4j.Logger;
 
 import backtype.storm.spout.Scheme;
@@ -12,6 +10,7 @@ import backtype.storm.tuple.Values;
 
 public class DemoScheme implements Scheme {
 
+  public static final String FIELD_ID = "id";
   public static final String FIELD_MESSAGE = "message";
   private static final long serialVersionUID = -2990121166902741545L;
   private static final Logger LOG = Logger.getLogger(TruckScheme.class);
@@ -19,14 +18,20 @@ public class DemoScheme implements Scheme {
   public List<Object> deserialize(byte[] bytes) {
     try {
       String message = new String(bytes, "UTF-8");
-      return new Values(message);
-    } catch (UnsupportedEncodingException e) {
+
+      // parse id from object
+      JSONObject obj = new JSONObject(message);
+      String id = obj.getString("_id");
+
+      return new Values(id, message);
+      
+    } catch (Exception e) {
       LOG.error(e);
       throw new RuntimeException(e);
     }
   }
 
   public Fields getOutputFields() {
-    return new Fields(FIELD_MESSAGE);
+    return new Fields(FIELD_ID, FIELD_MESSAGE);
   }
 }
