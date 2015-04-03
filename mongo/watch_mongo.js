98f2e8c36975cfda3f23097081c3fe4b158044f1
Jorge Chang <jorge@jorgechang.com>
2015-04-03 14:22:35 -0700

    for demo

--- mongo/watch_mongo.js
+++ mongo/watch_mongo.js
@@ -1,21 +1,24 @@
+// listen to changes in mongo oplog, and then push event to kafka
+
 var MongoOplog = require('mongo-oplog');
-var oplog = MongoOplog('mongodb://production-web-server.c.onefold-1.internal:27017/local', 'test.demo_messages').tail();
+var oplog = MongoOplog(
+		'mongodb://production-web-server.c.onefold-1.internal:27017/local',
+		'test.demo_messages').tail();
 
-var kafka = require('kafka-node'),
-     Producer = kafka.Producer,
-     client = new kafka.Client('hadoop-w-0.c.onefold-1.internal:2181'),
-     producer = new Producer(client);
+var kafka = require('kafka-node'), Producer = kafka.Producer, client = new kafka.Client(
+		'hadoop-w-0.c.onefold-1.internal:2181'), producer = new Producer(client);
 
-oplog.on('insert', function (doc) {
-  console.log(doc.ns + " inserted: %j", doc.o);
+oplog.on('insert', function(doc) {
+	console.log(doc.ns + " inserted: %j", doc.o);
 
-  payloads = [
-         { topic: 'truckevent2', messages: JSON.stringify(doc.o), partition: 0 },
-     ];
+	payloads = [ {
+		topic : 'demo',
+		messages : JSON.stringify(doc.o),
+		partition : 0
+	}, ];
 
-     producer.send(payloads, function(err, data){
-         console.log(data)
-     });
+	producer.send(payloads, function(err, data) {
+		console.log(data)
+	});
 
 });
-
--- src/main/java/com/hortonworks/tutorials/tutorial3/TruckEventProcessingTopology.java
+++ src/main/java/com/hortonworks/tutorials/tutorial3/TruckEventProcessingTopology.java
@@ -1,6 +1,5 @@
 package com.hortonworks.tutorials.tutorial3;
 
-
 import backtype.storm.Config;
 import backtype.storm.StormSubmitter;
 import backtype.storm.spout.SchemeAsMultiScheme;
@@ -20,133 +19,115 @@ import org.apache.storm.hdfs.bolt.sync.CountSyncPolicy;
 import org.apache.storm.hdfs.bolt.sync.SyncPolicy;
 
 /**
- *
+ * 
  * @author Kumar Kandasami
  */
-public class TruckEventProcessingTopology extends BaseTruckEventTopology 
-{
-    private static final String KAFKA_SPOUT_ID = "kafkaSpout"; 
-    private static final String HDFS_BOLT_ID = "hdfsBolt";
-    private static final String MONITOR_BOLT_ID = "monitorBolt";
-    private static final String HBASE_BOLT_ID = "hbaseBolt"; 
-    private static final String LOG_TRUCK_BOLT_ID = "logTruckEventBolt";
-            
-    public TruckEventProcessingTopology(String configFileLocation) throws Exception 
-    {
-        super(configFileLocation);
-    }
+public class TruckEventProcessingTopology extends BaseTruckEventTopology {
+  private static final String KAFKA_SPOUT_ID = "kafkaSpout";
+  private static final String HDFS_BOLT_ID = "hdfsBolt";
+  private static final String MONITOR_BOLT_ID = "monitorBolt";
+  private static final String HBASE_BOLT_ID = "hbaseBolt";
+  private static final String LOG_TRUCK_BOLT_ID = "logTruckEventBolt";
 
-    private SpoutConfig constructKafkaSpoutConf() 
-    {
-        BrokerHosts hosts = new ZkHosts(topologyConfig.getProperty("kafka.zookeeper.host.port"));
-        String topic = topologyConfig.getProperty("kafka.topic");
-        String zkRoot = topologyConfig.getProperty("kafka.zkRoot");
-        String consumerGroupId = "StormSpout";
+  public TruckEventProcessingTopology(String configFileLocation) throws Exception {
+    super(configFileLocation);
+  }
 
-        SpoutConfig spoutConfig = new SpoutConfig(hosts, topic, zkRoot, consumerGroupId);
+  private SpoutConfig constructKafkaSpoutConf() {
+    BrokerHosts hosts = new ZkHosts(topologyConfig.getProperty("kafka.zookeeper.host.port"));
+    String topic = topologyConfig.getProperty("kafka.topic");
+    String zkRoot = topologyConfig.getProperty("kafka.zkRoot");
+    String consumerGroupId = "StormSpout";
 
-        /* Custom TruckScheme that will take Kafka message of single truckEvent 
-         * and emit a 2-tuple consisting of truckId and truckEvent. This driverId
-         * is required to do a fieldsSorting so that all driver events are sent to the set of bolts */
-        spoutConfig.scheme = new SchemeAsMultiScheme(new TruckScheme());
+    SpoutConfig spoutConfig = new SpoutConfig(hosts, topic, zkRoot, consumerGroupId);
 
-        return spoutConfig;
-    }
-    
-    
-    
-    public void configureKafkaSpout(TopologyBuilder builder) 
-    {
-        KafkaSpout kafkaSpout = new KafkaSpout(constructKafkaSpoutConf());
-        int spoutCount = Integer.valueOf(topologyConfig.getProperty("spout.thread.count"));
-        builder.setSpout(KAFKA_SPOUT_ID, kafkaSpout);
-    }
-    
-    
-    public void configureHDFSBolt(TopologyBuilder builder) 
-    {
-        // Use pipe as record boundary
+    /*
+     * Custom TruckScheme that will take Kafka message of single truckEvent and
+     * emit a 2-tuple consisting of truckId and truckEvent. This driverId is
+     * required to do a fieldsSorting so that all driver events are sent to the
+     * set of bolts
+     */
+    spoutConfig.scheme = new SchemeAsMultiScheme(new TruckScheme());
 
-        String rootPath = topologyConfig.getProperty("hdfs.path");
-        String prefix = topologyConfig.getProperty("hdfs.file.prefix");
-        String fsUrl = topologyConfig.getProperty("hdfs.url");
-        String sourceMetastoreUrl = topologyConfig.getProperty("hive.metastore.url");
-        String hiveStagingTableName = topologyConfig.getProperty("hive.staging.table.name");
-        String databaseName = topologyConfig.getProperty("hive.database.name");
-        Float rotationTimeInMinutes = Float.valueOf(topologyConfig.getProperty("hdfs.file.rotation.time.minutes"));
+    return spoutConfig;
+  }
 
-        RecordFormat format = new DelimitedRecordFormat().withFieldDelimiter(",");
+  public void configureKafkaSpout(TopologyBuilder builder) {
+    KafkaSpout kafkaSpout = new KafkaSpout(constructKafkaSpoutConf());
+    int spoutCount = Integer.valueOf(topologyConfig.getProperty("spout.thread.count"));
+    builder.setSpout(KAFKA_SPOUT_ID, kafkaSpout);
+  }
 
-        //Synchronize data buffer with the filesystem every 100 tuples
-        SyncPolicy syncPolicy = new CountSyncPolicy(100);
+  public void configureHDFSBolt(TopologyBuilder builder) {
+    // Use pipe as record boundary
 
-        // Rotate data files when they reach five MB
-        //FileRotationPolicy rotationPolicy = new FileSizeRotationPolicy(5.0f, Units.MB);
+    String rootPath = topologyConfig.getProperty("hdfs.path");
+    String prefix = topologyConfig.getProperty("hdfs.file.prefix");
+    String fsUrl = topologyConfig.getProperty("hdfs.url");
+    String sourceMetastoreUrl = topologyConfig.getProperty("hive.metastore.url");
+    String hiveStagingTableName = topologyConfig.getProperty("hive.staging.table.name");
+    String databaseName = topologyConfig.getProperty("hive.database.name");
+    Float rotationTimeInMinutes = Float.valueOf(topologyConfig.getProperty("hdfs.file.rotation.time.minutes"));
 
-        //Rotate every X minutes
-        FileTimeRotationPolicy rotationPolicy = new FileTimeRotationPolicy
-                    (rotationTimeInMinutes, FileTimeRotationPolicy.Units.MINUTES);
+    RecordFormat format = new DelimitedRecordFormat().withFieldDelimiter(",");
 
-        //Hive Partition Action
-        HiveTablePartitionAction hivePartitionAction = new HiveTablePartitionAction
-                    (sourceMetastoreUrl, hiveStagingTableName, databaseName, fsUrl);
+    // Synchronize data buffer with the filesystem every 100 tuples
+    SyncPolicy syncPolicy = new CountSyncPolicy(100);
 
-        //MoveFileAction moveFileAction = new MoveFileAction().toDestination(rootPath + "/working");
+    // Rotate data files when they reach five MB
+    // FileRotationPolicy rotationPolicy = new FileSizeRotationPolicy(5.0f,
+    // Units.MB);
 
+    // Rotate every X minutes
+    FileTimeRotationPolicy rotationPolicy = new FileTimeRotationPolicy(rotationTimeInMinutes,
+        FileTimeRotationPolicy.Units.MINUTES);
 
+    // Hive Partition Action
+    HiveTablePartitionAction hivePartitionAction = new HiveTablePartitionAction(sourceMetastoreUrl,
+        hiveStagingTableName, databaseName, fsUrl);
 
-        FileNameFormat fileNameFormat = new DefaultFileNameFormat()
-                        .withPath(rootPath + "/staging")
-                        .withPrefix(prefix);
+    // MoveFileAction moveFileAction = new
+    // MoveFileAction().toDestination(rootPath + "/working");
 
-        // Instantiate the HdfsBolt
-        HdfsBolt hdfsBolt = new HdfsBolt()
-                         .withFsUrl(fsUrl)
-                 .withFileNameFormat(fileNameFormat)
-                 .withRecordFormat(format)
-                 .withRotationPolicy(rotationPolicy)
-                 .withSyncPolicy(syncPolicy)
-                 .addRotationAction(hivePartitionAction);
+    FileNameFormat fileNameFormat = new DefaultFileNameFormat().withPath(rootPath + "/staging").withPrefix(prefix);
 
-       // int hdfsBoltCount = Integer.valueOf(topologyConfig.getProperty("hdfsbolt.thread.count"));
-        builder.setBolt(HDFS_BOLT_ID, hdfsBolt, 2).shuffleGrouping(KAFKA_SPOUT_ID);
-    }
-    
-    public void configureHBaseBolt(TopologyBuilder builder)
-    {
-        TruckHBaseBolt hbaseBolt = new TruckHBaseBolt(topologyConfig);
-        builder.setBolt(HBASE_BOLT_ID, hbaseBolt, 2).shuffleGrouping(KAFKA_SPOUT_ID);
-    }
-    
-    
-    public void configureLogTruckEventBolt(TopologyBuilder builder)
-    {
-        LogTruckEventsBolt logBolt = new LogTruckEventsBolt();
-        builder.setBolt(LOG_TRUCK_BOLT_ID, logBolt).globalGrouping(KAFKA_SPOUT_ID);
-    }
-    
-    private void buildAndSubmit() throws Exception
-    {
-        TopologyBuilder builder = new TopologyBuilder();
-        configureKafkaSpout(builder);
-        //configureLogTruckEventBolt(builder);
-        configureHDFSBolt(builder);
-        
-        //configureHBaseBolt(builder);
-        
-        Config conf = new Config();
-	conf.setDebug(true);
-        
-        StormSubmitter.submitTopology("truck-event-processor", 
-                                    conf, builder.createTopology());
-    }
+    // Instantiate the HdfsBolt
+    HdfsBolt hdfsBolt = new HdfsBolt().withFsUrl(fsUrl).withFileNameFormat(fileNameFormat).withRecordFormat(format)
+        .withRotationPolicy(rotationPolicy).withSyncPolicy(syncPolicy).addRotationAction(hivePartitionAction);
 
-    public static void main(String[] str) throws Exception
-    {
-        String configFileLocation = "truck_event_topology.properties";
-        TruckEventProcessingTopology truckTopology 
-                = new TruckEventProcessingTopology(configFileLocation);
-        truckTopology.buildAndSubmit();
-    }
+    // int hdfsBoltCount =
+    // Integer.valueOf(topologyConfig.getProperty("hdfsbolt.thread.count"));
+    builder.setBolt(HDFS_BOLT_ID, hdfsBolt, 2).shuffleGrouping(KAFKA_SPOUT_ID);
+  }
+
+  public void configureHBaseBolt(TopologyBuilder builder) {
+    TruckHBaseBolt hbaseBolt = new TruckHBaseBolt(topologyConfig);
+    builder.setBolt(HBASE_BOLT_ID, hbaseBolt, 2).shuffleGrouping(KAFKA_SPOUT_ID);
+  }
+
+  public void configureLogTruckEventBolt(TopologyBuilder builder) {
+    LogTruckEventsBolt logBolt = new LogTruckEventsBolt();
+    builder.setBolt(LOG_TRUCK_BOLT_ID, logBolt).globalGrouping(KAFKA_SPOUT_ID);
+  }
+
+  private void buildAndSubmit() throws Exception {
+    TopologyBuilder builder = new TopologyBuilder();
+    configureKafkaSpout(builder);
+    // configureLogTruckEventBolt(builder);
+    configureHDFSBolt(builder);
+
+    // configureHBaseBolt(builder);
+
+    Config conf = new Config();
+    conf.setDebug(true);
+
+    StormSubmitter.submitTopology("truck-event-processor", conf, builder.createTopology());
+  }
+
+  public static void main(String[] str) throws Exception {
+    String configFileLocation = "truck_event_topology.properties";
+    TruckEventProcessingTopology truckTopology = new TruckEventProcessingTopology(configFileLocation);
+    truckTopology.buildAndSubmit();
+  }
 
 }
